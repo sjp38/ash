a1577979b69400e979a7d762e98f44522cfe2afe
SeongJae Park <sj38.park@gmail.com>
2012-10-15 04:34:17 +0900

    Implement record, event feature

--- ashval.py
+++ ashval.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # ash evaluator.
 
+import copy
 import sys
+import time
 
 import data
+#TODO: Make monkey-dependant part unittest-able
 import devmgr
 
-# TODO: remove below map. Let's use file for default operator maps, too.
-DEFAULT_OPERATOR_MAPS = {
-        "listDevices": lambda args: "listDevices %s" % args,
-        "testFunction:": lambda args: "c. args : %s" % args,
-        "touch": lambda args: "touch. args : %s" % args,
-        "c": lambda args: "c. args : %s" % args,
-        "c": lambda args: "c. args : %s" % args
-        }
+RECORD_FILTER = ("arg", "record", "record_stop")
 
 # Make raw expression(python string) to list(python list with strings)
 def _raw_ashval(expr):
@@ -46,19 +42,65 @@ def arg(number):
         number = int(number)
     return _argstack[-2][number - 1]
 
+_recording = False
+_record_queue = []
+_record_name = ""
+_last_record_time = 0
+
+def record(record_name):
+    if not isinstance(record_name, str) or len(record_name) <= 0:
+        now = time.localtime()
+        record_name = "ash_record_%04d-%02d-%02d-%02d-%02d-%02d" % (
+                now.tm_year, now.tm_mon, now.tm_mday,
+                now.tm_hour, now.tm_min, now.tm_sec)
+    global _record_queue
+    global _record_name
+    global _recording
+    _record_queue = []
+    _record_name = record_name
+    _recording = True
+
+def record_stop(record_name=None):
+    global _record_queue
+    global _record_name
+    global _recording
+
+    if isinstance(record_name, str) and len(record_name) > 0:
+        _record_name = record_name
+    _recording = False
+    data.add_callback("", "alias", _record_name, copy.deepcopy(_record_queue))
+    _record_queue = []
+
 # just for test
 def _mock_function(a, b, c):
     return "_mock_function : %s, %s, %s" % (a, b, c)
 
+def event(mode, type_, value, *arg):
+    code = data.get_callback(mode, type_, value)
+    _argstack[-1] = _argstack[-1][3:]
+    result = None
+    for expr in code:
+        result = ashval(expr, False)
+    return result
+
 def exit():
     sys.exit()
 
+def sleep(time_):
+    if not isinstance(time_, float):
+        try:
+            time_ = float(time_)
+        except:
+            "Sleep fail! can't convert argument to float"
+    time.sleep(time_)
+
 # Return ash function(list) or python function object.
 def _get_code(expr):
-    if not isinstance(expr[0], str):
+    print "_get_code. expr : %s" % expr
+    if not isinstance(expr, list) or not isinstance(expr[0], str):
         return False
     for module in [data, sys.modules[__name__], devmgr]:
-    #for module in [data, sys.modules[__name__]]:
+#    for module in [data, sys.modules[__name__]]:
         if hasattr(module, expr[0]):
             f = getattr(module, expr[0])
             if hasattr(f, '__call__'):
@@ -77,8 +119,6 @@ def ashval(expr, is_raw = True):
     result = expr
 
     # if this is executable, do it(eval arguments, call function)
-    # call function means... eval or calling real bottom layer function.
-    # if this is function call, do it(eval arguments, start function)
     code = _get_code(expr)
     if not code or code == data.NO_CALLBACK:
         return result
@@ -92,6 +132,16 @@ def ashval(expr, is_raw = True):
             result = ashval(expr, False)
     else:
         result = code(*args)
+        if _recording and not expr[0] in RECORD_FILTER:
+            global _last_record_time
+            global _record_queue
+            interval = time.time() - _last_record_time
+            if len(_record_queue) > 0 and interval > 0.3:
+                _record_queue.append(["sleep", interval])
+            expr = [expr[0]] + copy.deepcopy(args)
+            _record_queue.append(expr)
+            _last_record_time = time.time()
+
     del _argstack[-1]
 
     return result
--- test_ashval.py
+++ test_ashval.py
@@ -32,7 +32,6 @@ class TestAshval(unittest.TestCase):
                     ["_mock_function",
                         ["arg", "1"], ["arg", "2"], ["arg", "3"]]
                 ])
-        print data._callbacks[data.DEFAULT_EVENT_MODE]["alias"]
         self.assertEqual(ashval.ashval("c 100 200 300"),
                 "_mock_function : 100, 200, 300")
         self.assertEqual(ashval.ashval("c 110 0 350"),
@@ -45,7 +44,6 @@ class TestAshval(unittest.TestCase):
                     ["_mock_function",
                         ["arg", "2"], ["arg", "3"], ["arg", "1"]]
                 ])
-        print data._callbacks[data.DEFAULT_EVENT_MODE]["alias"]
         self.assertEqual(ashval.ashval("d 100 200 300"),
                 "_mock_function : 200, 300, 100")
         self.assertEqual(ashval.ashval("d 110 0 350"),
@@ -66,3 +64,29 @@ class TestAshval(unittest.TestCase):
 
         self.assertTrue(ashval.ashval("a b c [[d] [e]]"))
         self.assertTrue(ashval.ashval("_mock_function [[d] [e]] f g"))
+
+    def test_record(self):
+        data.add_callback("", "alias", "c",
+                [
+                    ["_mock_function",
+                        ["arg", "1"], ["arg", "2"], ["arg", "3"]]
+                ])
+
+        ashval.ashval("record abc")
+        ashval.ashval("c 100 200 300")
+        ashval.ashval("c 200 200 300")
+        ashval.ashval("c 100 200 300")
+        ashval.ashval("record_stop")
+        self.assertEqual(ashval.ashval("abc"),
+                "_mock_function : 100, 200, 300")
+
+
+    def test_event(self):
+        data.clear()
+        data.add_callback("", "typeB", "valueC",
+                [
+                    ["_mock_function",
+                        ["arg", "1"], ["arg", "2"], ["arg", "3"]]
+                ])
+        self.assertEqual(ashval.ashval("event default typeB valueC 10 20 33"),
+                "_mock_function : 10, 20, 33")
